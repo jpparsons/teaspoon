c315a4ca549161dab7ea3cd1f0e1fee4729c89ae
John Parsons <johnparsons@john.local>
2017-04-30 09:53:19 -0700

    Add log

--- lib/teaspoon/driver/phantomjs.rb
+++ lib/teaspoon/driver/phantomjs.rb
@@ -32,6 +32,7 @@ module Teaspoon
       protected
 
       def run(*args, &block)
+        log("ARGS: #{*args}")
         IO.popen([executable, *args].join(" ")) { |io| io.each(&block) }
 
         unless $?.nil? || $?.success?
